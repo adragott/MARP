a2e5263842fa14fb6ac2f9d9eb060d89f532d319
Penguin <ad0097@uah.edu>
2018-12-15 19:18:37 -0600

    Added sd card holder, logic level shifter, fets for power source logic; removed old stuff

--- MARP_Software/MARP_THERMOCOUPLE_TESTING_12_15_18/MARP_THERMOCOUPLE_TESTING_12_15_18/src/devices/thermocouple.c
+++ MARP_Software/MARP_THERMOCOUPLE_TESTING_12_15_18/MARP_THERMOCOUPLE_TESTING_12_15_18/src/devices/thermocouple.c
@@ -4,3 +4,10 @@
  * Created: 12/15/2018 2:21:09 PM
  *  Author: Penguin
  */ 
+
+#include "thermocouple.h"
+
+void thermocouple_init(uint8_t ID)
+{
+	
+}
--- MARP_Software/MARP_THERMOCOUPLE_TESTING_12_15_18/MARP_THERMOCOUPLE_TESTING_12_15_18/src/devices/thermocouple.h
+++ MARP_Software/MARP_THERMOCOUPLE_TESTING_12_15_18/MARP_THERMOCOUPLE_TESTING_12_15_18/src/devices/thermocouple.h
@@ -8,7 +8,20 @@
 #ifndef _THERMOCOUPLE_H_
 #define _THERMOCOUPLE_H_
 
+#include <asf.h>
 
+#define MAX_THERMO_SAMPLE_COUNT 64
 
+typedef struct thermocouple
+{
+	uint8_t id;
+	float voltage[MAX_THERMO_SAMPLE_COUNT];
+	float temp[MAX_THERMO_SAMPLE_COUNT];
+}thermocouple;
 
+void thermocouple_init(uint8_t ID);
+
+void fill_thermo_data(void);
+
+void get_thermo_data(thermocouple* inst);
 #endif
